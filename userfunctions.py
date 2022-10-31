c9077bb4cbf9f184b2cec88d633b44726d201b5f
Peter <45223204+ysm463647286@users.noreply.github.com>
2022-10-30 22:55:34 -0600

    Implementation of search_a
    some of the firgures need to be updated later

--- userfunctions.py
+++ userfunctions.py
@@ -58,7 +58,48 @@ def search_ps(uid):
             for i in range(0, 5):
                 print(new_data[i])
             break
-            
+
+def search_a(uid):
+    keywords = []
+    index = 0
+    while True:
+        keywords.append(input("Please enter the keyword you would like to search: "))
+        c = input("Continue? Y/N\n")
+        index += 1
+        if c == 'Y':
+            continue
+        else:
+            data = []
+            # search songs and playlists with keywords
+            for i in range(0, index):
+                login.cursor.execute(
+                    """SELECT a.name,a.nationality,COUNT(p2.sid) AS c FROM artists a,perform p1, perform p2,songs s WHERE s.title LIKE ? AND p1.sid=s.sid AND a.aid=p1.aid AND p2.aid=p1.aid;""",
+                    ('%' + keywords[i] + '%',))
+                if login.cursor.fetchall()[0] is not None:
+                    data += login.cursor.fetchall()
+                login.cursor.execute(
+                    """SELECT a.name,a.nationality,COUNT(p.sid) AS c FROM artists a,perform p WHERE a.name LIKE ? AND p.aid=a.aid;""",
+                    ('%' + keywords[i] + '%',))
+                if login.cursor.fetchall()[0] is not None:
+                    data += login.cursor.fetchall()
+            # deal with duplicates
+            new_data = []
+            for info in data:
+                amounts = 0
+                for k in keywords:
+                    if k in info[1]:
+                        amounts += 1
+                if info not in new_data:
+                    new_data.append([info, amounts])
+                else:
+                    for j in new_data:
+                        if j[0] == info:
+                            j[1] += 1
+            # sorted by amount of keywords contained
+            new_data.sort(key=takeSecond, reverse=True)
+            for i in range(0, 5):
+                print(new_data[i][0])
+            break
 
 def menu(uid):
     """Main screen of the program, also the login screen"""
